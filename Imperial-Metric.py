c9d5b1ee22fba4e7fdfcae791720e782af5c3191
tma-7 <talha.ahd2007@gmail.com>
2025-08-13 00:42:20 -0400

    Update Imperial-Metric.py

--- Imperial-Metric.py
+++ Imperial-Metric.py
@@ -6,7 +6,7 @@ ask_question = input("What unit are you giving me? | 'I' for Imperial | 'M' for 
 #--------------------------------------------------------------------------------------------------------------------------------------
 #COVERTING METRIC TO IMPERIAL
 if ask_question == "M":
-    get_metric = input("What are you converting? A = kg to lbs | B = m to ft | C = cm to in | D = km to mi: ")
+    get_metric = input("What are you converting? A = kg to lbs | B = m to ft | C = cm to in | D = km to mi | E = C to F: ")
     
     if get_metric == "A":
         kg = float(input("What is the weight?"))
@@ -28,13 +28,18 @@ if ask_question == "M":
         mile =  km / 1.609
         print(f"The distance in inches is {round(mile, 1)} mi.")
     
+    elif get_metric == 'E':
+        celcius = float(input("What is the temperature?: "))
+        farenheit = (celcius * (9/5) + 32)
+        print(f"The temperature in farenheit is {round(farenheit, 1)} F.")
+    
     else:
         print(f"{get_metric} is Invalid.")
 
 #--------------------------------------------------------------------------------------------------------------------------------------
 #CONVERTING IMPERIAL TO METRIC
 if ask_question == "I":
-    get_imperial = input("What are you converting? A = lbs to kg | B = ft to m | C = in to cm | D = mi to km: ")
+    get_imperial = input("What are you converting? A = lbs to kg | B = ft to m | C = in to cm | D = mi to km | E = F to C: ")
     
     if get_imperial == "A":
         pound_1 = float(input("What is the weight?"))
@@ -55,10 +60,15 @@ if ask_question == "I":
         mile_1 = float(input("What is the distance?: "))
         km_1 =  mile_1 * 1.609
         print(f"The distance in kilometers is {round(km_1, 1)} km.")
-        
+
+    elif get_imperial == 'E':
+        farenheit_1 = float(input("What is the temperature?: "))
+        celcius_1 = (farenheit_1 - 32) * (5/9)
+        print(f"The temperature in celcius is {round(celcius_1, 1)} C.")
+
     else:
         print(f"{get_imperial} is Invalid.")
 
 else:
     print("Invalid Option")
-
+    
